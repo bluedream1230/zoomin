c19a129d12ee4730efbec2e229341c0d5047da5d
bluedream1230 <bluedream921230@gmail.com>
2022-12-20 19:15:01 +0900

    fix: header update

--- src/services/api.js
+++ src/services/api.js
@@ -71,7 +71,12 @@ class Api {
         const sendRequest = (axiosInstance) => {
             const url = Api.replaceVariables(route, params);
             const headers = {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+                'Access-Control-Allow-Headers':
+                    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
             };
 
             if (state.auth.token) {
